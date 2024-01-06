4333d5d0960405cf7ae7aca9a1ccade128ac0285
PrithveeOjha <ojhaprithvee@gmail.com>
2024-01-06 12:39:28 +0530

    new version update
    updating the mini project

---
+++ lib/view/spalsh.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: const [
+          SizedBox(height: 300,),
+
+          
+          FadeInImage(
+            
+            placeholder : AssetImage("assets/img/logo.png"),
+          image: AssetImage("assets/img/logo.png") ,
+          
+          height: 230 , width: 230,),
+
+        ],
+      )),
+    );
+  }
+}
